dae527cf84f692d803403d475d99805c474057a0
Chitra Bandari <50966@hexaware.com>
2021-09-01 17:39:32 +0530

    in flight

--- src/app/admin/in-flight/in-flight.component.ts
+++ src/app/admin/in-flight/in-flight.component.ts
@@ -11,13 +11,14 @@ import { AppService } from 'src/app/services/app.service';
 export class InFlightComponent implements OnInit {
 
   @ViewChild("seatModal", { static: false }) seatModal: ElementRef
-  passengers: any[]=[];
+  passengers: any[] = [];
   p: number = 1;
   dropdownSettings = {};
   editForm: FormGroup;
+  allservices: any[]=[]
 
-  dropdownList = [{Id:1,Services: "Meals"},{Id:2,Services:"Small Meals"},{Id:3,Services:"Baggage"},{Id:4,Services:"Drinks"},{Id:5,Services:"Water"},
-  {Id:6,Services: "Perfume"},{Id:7,Services: "Snacks"},{Id:8,Services: "Pizza"},{Id:9,Services: "Chocolates"}];
+  dropdownList = [{ Id: 1, Services: "Meals" }, { Id: 2, Services: "Small Meals" }, { Id: 3, Services: "Baggage" }, { Id: 4, Services: "Drinks" }, { Id: 5, Services: "Water" },
+  { Id: 6, Services: "Perfume" }, { Id: 7, Services: "Snacks" }, { Id: 8, Services: "Pizza" }, { Id: 9, Services: "Chocolates" }];
 
   flightSeatsSequence = [
     ['1A', '1B', '1C', '1D', '1E', '1F'],
@@ -31,8 +32,9 @@ export class InFlightComponent implements OnInit {
   ]
   mealseat: any;
   selectedmeal: any;
+  selectedseat: any;
 
-  constructor(private service: AppService, private fb: FormBuilder,private router: Router) { }
+  constructor(private service: AppService, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
     this.dropdownSettings = {
@@ -44,112 +46,140 @@ export class InFlightComponent implements OnInit {
     };
 
     this.editForm = this.fb.group({
-      'name': ['', Validators.required],
-      'number': ['', Validators.required],
-      'dob': ['', Validators.required],
       'services': ['', Validators.required],
-      'address': ['', Validators.required]
     })
 
- this.getPassengers()
-   
+    this.getPassengers()
+
   }
 
-  getPassengers(){
-    this.service.getDashboard_data().subscribe(data=>{
+  getPassengers() {
+    this.service.getDashboard_data().subscribe(data => {
       console.log(data);
       this.passengers = data;
     })
   }
 
-  checkPassengerMeals(seat){
-    
-    var detailsbasedonseat = this.passengers.filter(x=>x.seatNumber == seat)
-    return detailsbasedonseat[0]?.meals;
-  }
-  checkPassengerSmallMeals(seat){
-    
-    var detailsbasedonseat = this.passengers.filter(x=>x.seatNumber == seat)
-    return detailsbasedonseat[0]?.smallmeals;
-  }
 
-  close() {
-    this.editForm.reset()
+  checkPassengerMeals(seat) {
+
+    var detailsbasedonseat = null;
+    var data = this.passengers
+    data.filter(x => {
+      if (x.seatNumber == seat) {
+        detailsbasedonseat = x?.meals
+      }
+    })
+    return detailsbasedonseat;
   }
 
-  // openModal(){
-  //   console.log("hi")
-  //   this.seatModal.nativeElement.click()
-  // }
-  AddorEdit() {
+  checkPassengerSmallMeals(seat) {
+    var detailsbasedonseat = null;
+    var data = this.passengers
+    data.filter(x => {
+      if (x.seatNumber == seat) {
+        detailsbasedonseat = x?.smallmeals
+      }
+    })
+    return detailsbasedonseat;
 
-    if (this.editForm.valid) {
-      console.log(this.editForm.value)
 
-      this.passengers.push({ 
-      "passengerName": this.editForm.value.name, 
-      "services": this.editForm.value.services.map(x => x.Services).toString(),
-     })
-      
-      console.log(this.editForm.value.services)
-      alert('Details Added')
-      this.seatModal.nativeElement.click();
-      this.editForm.reset()
+  }
+
+
+
+  getSelectedSeat(seat) {
+    this.selectedseat = seat;
+    // console.log(this.checkPassengerMeals(this.selectedseat))
+    if (this.checkPassengerMeals(this.selectedseat) == null && this.checkPassengerSmallMeals(this.selectedseat) == null) {
+      alert("This seat is not booked by any passenger, so you cannot opt or change your services")
+    }
+    else{
+
     
+    if ( this.checkPassengerSmallMeals(this.selectedseat) == "Yes") {
+      if (confirm("Small Meal is already opted. Are you sure you want to remove this small meal?")) {
+        this.passengers = this.passengers.filter(x => {
+       
+          if (x.seatNumber == seat) {
+            var allservices = x.services.split(",")
+           allservices = allservices.filter(y=>y != "Small Meals")
+          
+           x.smallmeals ="No"
+           x.services = allservices.toString()
+          
+          }
+          
+          return x;
+        })
+      }
+    }
+  
+    if(this.checkPassengerMeals(this.selectedseat) == "Yes"){
+      if (confirm("Meal is already opted. Are you sure you want to remove this meal?")) {
+        this.passengers = this.passengers.filter(x => {
+       
+          if (x.seatNumber == seat) {
+            var allservices = x.services.split(",")
+           allservices = allservices.filter(y=>y != "Meals")
+          
+           x.meals ="No"
+           x.services = allservices.toString()
+          }
+          this.getSelectedSeat(x)
+          return x;
+        })
+      }
+    }
 
-    } else {
+    
+    
+        
+    
+  
+  }
+      // else {
+      //   this.seatModal.nativeElement.click()
+
+      // }
+  
+
+  }
+
+  AddMeals() {
+    var passengerseat = this.passengers.filter(x => x.seatNumber == this.selectedseat)
+     if(this.editForm.valid){
+        this.passengers = this.passengers.filter(x => {
+          if (x.seatNumber == this.selectedseat) {
+            
+            this.allservices = x.services.split(",")
+  
+            this.allservices.push(" Small Meals")
+           x.smallmeals ="Yes"
+           x.services = this.allservices.join()
+
+          }
+          return x;
+        })
+        this.seatModal.nativeElement.click()
+        this.editForm.reset()
+    }
+    else{
       for (var landingformvalues in this.editForm.controls) {
         this.editForm.controls[landingformvalues].markAllAsTouched();
       }
     }
   }
 
-  
   onItemSelect(item: any) {
-    console.log(item);
+    // console.log(item);
   }
   onSelectAll(items: any) {
-    console.log(items);
+    // console.log(items);
+  }
+  close() {
+    this.editForm.reset()
   }
 
-  getSelectedMeal(seat){
-    this.selectedmeal = seat;
-    if(this.checkPassengerMeals(this.selectedmeal) != null){
-     if( confirm("Meal is already opted. Are you sure you want to remove this meal?")){
-      this.passengers = this.passengers.filter(x=>{
 
-        if( x.seatNumber == seat){
-          x.seatNumber = ""
-        }
-        return x;
-     })
-    }
-    }else{
-      this.seatModal.nativeElement.click()
-    }
-  }
-
-  AddMeals(i){
-    var selectedmeals = this.passengers.filter(x=>x.passengerName == i)
-
-    if(selectedmeals.length != 0){
-        if(selectedmeals[0].seatNumber ==""){
-          this.passengers = this.passengers.filter(x=>{
-            return x.seatNumber = x.passengerName == i ? this.selectedmeal : x.seatNumber
-          })
-          this.seatModal.nativeElement.click()
-        }else{
-          if(confirm("Meal is already opted by user. Are you sure to remove meal " + selectedmeals[0].seatNumber
-          + " and allocate meal " + this.selectedmeal)){
-            this.passengers = this.passengers.filter(x=>{
-              return x.seatNumber = x.passengerName == i ? this.selectedmeal : x.seatNumber
-            })
-          }else{
-            this.seatModal.nativeElement.click()
-          }
-        }
-    }else{
-      alert("Invalid Action!")
-    }
-  }
 }
